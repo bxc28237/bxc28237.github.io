43c9f490e73ba5a2809db21c2e6385bad22913d6
bxc28237 <bxc28237@hawkmail.hacc.edu>
2024-12-09 15:32:49 -0500

    nav bar

---
+++ projects/project-4/script.js
@@ -0,0 +1,7 @@
+// Toggle the navigation menu on click of the hamburger
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
