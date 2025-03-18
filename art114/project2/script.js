9a3f6668e28f30aa3a5a3e403c4d5a44891fef8c
bxc28237 <bxc28237@hawkmail.hacc.edu>
2025-03-18 19:45:07 -0400

    1

--- art114/project2/script.js
+++ art114/project2/script.js
@@ -1,8 +1,6 @@
-document.getElementById('hamburger-btn').addEventListener('click', function() {
-    const navLinks = document.querySelector('.nav-links');
-    const hamburger = document.getElementById('hamburger-btn');
-  
-    navLinks.classList.toggle('active');
-    hamburger.classList.toggle('active');
-  });
-  
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+});
